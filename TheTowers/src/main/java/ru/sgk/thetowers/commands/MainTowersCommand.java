84bb37d9220e2bf8326dc5fe5aa264417614f514
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-06 01:59:52 +0700

    Команды
    Добавил метод hasPermission(CommandSender, String), используй его, при проверке прав.
    Сделал небольшую основу для вывода хелпы, так же немного изменил конфиг с хелпой

--- TheTowers/src/main/java/ru/sgk/thetowers/commands/MainTowersCommand.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/commands/MainTowersCommand.java
@@ -1,35 +1,116 @@
 package ru.sgk.thetowers.commands;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import ru.sgk.thetowers.data.Configurations;
+import ru.sgk.thetowers.game.data.troops.AbstractTroop;
+import ru.sgk.thetowers.game.data.troops.TroopPhantom;
 import ru.sgk.thetowers.utils.Logs;
 
+import java.util.List;
+
+
 public class MainTowersCommand implements CommandExecutor {
+    private static List<AbstractTroop> troops = Lists.newArrayList();
+
+    /**
+     *
+     * @param sender
+     * @param perm
+     * @return Возвращает true, если sender имеет право perm.
+     * Так же возвращает true, если sender имеет право towers.dev или towers.admin.
+     * В любом другом случае, возвращает false
+     */
+    public boolean hasPermission(CommandSender sender, String perm){
+        return sender.hasPermission("towers.dev") || sender.hasPermission("towers.admin") || sender.hasPermission(perm);
+    }
+
+    /**
+     *
+     * @param sender
+     * @param type - Тип хелпы команд, которые выводить: help, arena, player, misc
+     */
+    public void printHelp(CommandSender sender, String type) {
+        if (type.equalsIgnoreCase("arena"))
+        {
+            if (hasPermission(sender, "towers.arenas"))
+            {
+                // TODO: вывод хелпы по аренам
+            }
+            else {
+                // TODO: вывод сообщения о недостатке прав
+            }
+        }
+        else if (type.equalsIgnoreCase("help"))
+        {
+
+            if  (   !hasPermission(sender, "towers.arenas") &&
+                    // Если так же не имеет других прав, кроме
+                    hasPermission(sender, "towers.player")
+                )
+            {
+                printHelp(sender, "player");
+            }
+            else {
+                if (hasPermission(sender, "towers.help"))
+                {
+                    sender.sendMessage(Configurations.getLocaleString("commands.towers.help.help"));
+                }
+                if (hasPermission(sender, "towers.arenas"))
+                {
+                    sender.sendMessage(Configurations.getLocaleString("commands.towers.help.arena"));
+                }
+                if (hasPermission(sender, "towers.player"))
+                {
+                    sender.sendMessage((Configurations.getLocaleString("commands.towers.help.player")));
+                }
+            }
+        }
+        else if (type.equalsIgnoreCase("player"))
+        {
+            /*
+            TODO:
+                Вывод команд для игрока.
+                Здесь так же добавь forceJoin
+                или как там оно, но с проверкой прав.
+             */
+        }
+        else if (type.equalsIgnoreCase("misc"))
+        {
+            if (hasPermission(sender, "towers.help.misc"))
+            {
+                // TODO: Вывод команд без категорий, так же с правами всё
+            }
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!(sender instanceof Player)) {
-            sender.sendMessage(Configurations.getLocaleString("console"));
-            return false;
-        }
-
-        if(args.length < 1) {
-            for(String string : Configurations.getLocaleStringList("commands.towers.help")) {
-                sender.sendMessage(string);
+        if (command.getName().equalsIgnoreCase("towers")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(Configurations.getLocaleString("console"));
+                return false;
             }
-            return false;
-        }
 
-        //Arenas
-        if(args[0].equalsIgnoreCase("arena")) {
-            if(args.length < 2) {
-                for(String string : Configurations.getLocaleStringList("commands.towers.arena")) {
+            if (args.length < 1) {
+                for (String string : Configurations.getLocaleStringList("commands.towers.help")) {
                     sender.sendMessage(string);
                 }
-            } else if(args[1].equalsIgnoreCase("2")){
+                return false;
+            }
+            //Arenas
+            if (args[0].equalsIgnoreCase("arena")) {
+                if (args.length < 2) {
+                    for (String string : Configurations.getLocaleStringList("commands.towers.arena")) {
+                        sender.sendMessage(string);
+                    }
+                } else if (args[1].equalsIgnoreCase("2")) {
 
+                }
             }
         }
 
