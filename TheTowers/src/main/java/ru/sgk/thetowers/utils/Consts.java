f2d9db7f526782fc8c93cd286adf8f76f5c0f993
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2019-09-06 21:08:03 +0300

    Изменен & на §

--- TheTowers/src/main/java/ru/sgk/thetowers/utils/Consts.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/utils/Consts.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 
 public class Consts {
-	public static final String PREFIX = "&8[&7&lTheTowers&8]&3";
+	public static final String PREFIX = "§8[§7§lTheTowers§8]§3";
 	public static final Logger logger = Bukkit.getLogger();
 	public static final String DEBUG_PREFIX = PREFIX + " §c§l[DEBUG]";
 }
