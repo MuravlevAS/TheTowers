05185bf609bcdfa4e846f2b80dfae2158ac38474
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-13 00:09:37 +0700

    GameProcess.java Комментарии

--- TheTowers/src/main/java/ru/sgk/thetowers/game/GameProcess.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/game/GameProcess.java
@@ -5,34 +5,51 @@ import ru.sgk.thetowers.game.data.arenas.GameArenas;
 import ru.sgk.thetowers.utils.GameScheduler;
 
 public class GameProcess {
-
+	/** id потока игрового процесса */
 	private int threadId;
-	private long counter;
-	private long globalCounter;
+	/** Главный счётчик игрового процесса (в мс) */
+	private int mainCounter;
+	/** Количество секунд, которые идёт игра */
 	private int seconds;
+	/** Количество минут, которые идёт игра */
 	private int minutes;
-	private long tmpTimer;
+	/** Промежуточный таймер. Нужен для всякой херни */
+	private int tmpTimer;
+	/** Включён ли промежуточный таймер */
+	private boolean tmpTimerOn;
+	/** Арена, на которой идёт игровой процесс */
 	private GameArena arena;
+	/** Начата ли игра. Т.е истёк ли таймер отсрочки до начала игры*/
 	private boolean started;
+	/** Время (в мс), после истечения которого начинается игра. */
 	private int startedTime;
 	public GameProcess(String arena)
 	{
 		this.arena = GameArenas.getArena(arena);
 		this.startedTime = 10_000;
 	}
-
+	/**
+	 * запускает игровой процесс.
+	 */
 	public void start()
 	{
+		// обнуляем всех переменных  
 		tmpTimer = 0;
-		counter = 0;
+		mainCounter = 0;
 		seconds = 0;
+
 		started = false;
 		threadId = GameScheduler.scheduleTask(this::update, 0);
 	}
 
+	/**
+	 * Останавливает игровой процесс
+	 */
 	public void stop()
 	{
+		// TODO Остановка игрового процесса
 		GameScheduler.cancel(threadId);
+		
 	}
 
 	/**
@@ -40,25 +57,30 @@ public class GameProcess {
 	 */
 	private void update()
 	{
-		if (counter % 1000 == 0)
+		// counter прибавляется каждую мс. При достижении 1к, секунды увеличиваются на 1
+		if (mainCounter % 1000 == 0)
 		{
 			seconds++;
-			counter = 0;
+			mainCounter = 0;
 		}
+		// Если игра ещё не начата
 		if (!started)
 		{
-			if (globalCounter >= startedTime) started = true;
+			// И таймер больше чем время старта, стартуем игру
+			if (mainCounter >= startedTime) started = true;
 		}
 		else
 		{
 			// TODO: game process;
 		}
-		globalCounter++;
-		counter++;
+		
+		// Обновление счётчиков
+		mainCounter++;
+		if (tmpTimerOn) tmpTimer++;
 	}
-
-	public long getGlobalCounter(){
-		return this.globalCounter;
-	}
+	/**
+	 * 
+	 * @return глобальный счётчик игры.
+	 */
 
 }
