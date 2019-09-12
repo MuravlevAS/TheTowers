3d1a125a44dffa248205c8b81f07476f488d628d
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-13 02:10:34 +0700

    GameArena.java Комментарии

--- TheTowers/src/main/java/ru/sgk/thetowers/game/data/arenas/GameArena.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/game/data/arenas/GameArena.java
@@ -1,21 +1,27 @@
 package ru.sgk.thetowers.game.data.arenas;
 
-import com.google.common.collect.Lists;
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+
+import com.google.common.collect.Lists;
+
 import ru.sgk.thetowers.data.Configurations;
 import ru.sgk.thetowers.game.data.teams.GameTeam;
 import ru.sgk.thetowers.game.data.teams.GameTeamArea;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GameArena
 {
+	/** Команды, которые есть в арене */
     private List<GameTeam> teams;
+    /** Максимум игроков, которые могут зайти в игру*/ 
     private int maxPlayers;
+    /** Локация лобби */
     private Location lobbyLocation;
+    /** Название арены */
     private String arenaName;
+    /** Количество игроков в команде */
     private int teamSize;
 
     public GameArena(String arenaName){
@@ -26,45 +32,54 @@ public class GameArena
     public GameArena()
     {
         this.teamSize = 1;
-        teams = new ArrayList<>();
+        teams = Lists.newArrayList();
     }
-
+    
     public void removeTeam(GameTeam team)
     {
         if (teams.remove(team))
             this.maxPlayers-=teamSize;
     }
-
-
+    
     public void addTeam(GameTeam team) {
         if (!teams.contains(team)) {
             teams.add(team);
             this.maxPlayers+=teamSize;
         }
     }
-
+    
     public List<GameTeam> getTeams() {
         return teams;
     }
-
+    /**
+     * Сохраняет арену в конфиг.
+     * После выполнения этого метода, сохраните конфигурацию, используя метод {@link #GameArenas.saveConfig() GameArenas.saveConfig()} 
+     */
     public void saveToConfig(){
 
         if (teamSize <= 0) teamSize = 1;
+        // Установка в конфиге имени арены 
         GameArenas.getConfig().set("arenas." + arenaName + ".team-size", teamSize);
         if (lobbyLocation != null)
             GameArenas.getConfig().set("arenas." + arenaName + ".lobby-location", lobbyLocation);
+        // Проходим по имеющимся командам.
         for (GameTeam team : teams)
         {
+        	// Получаем все переменные из команды
             String teamColor = team.getColor().toString();
             Location troopsSpawn = team.getTroopSpawn();
             Location troopsEnd = team.getTroopsEnd();
             List<Location> troopsWay = team.getTroopWay();
             List<Location> towerBlocks = Lists.newArrayList();
+            // Преобразуем блоки для таверов в массив их локациями
             if (team.getTowerBlocks()!=null)
-            for (Block block : team.getTowerBlocks())
             {
-                towerBlocks.add(block.getLocation());
+	            for (Block block : team.getTowerBlocks())
+	            {
+	                towerBlocks.add(block.getLocation());
+	            }
             }
+            // Получаем область команды
             GameTeamArea area = team.getArea();
             Location min = null, max = null, spawn = null;
             if (area !=null)
@@ -73,6 +88,7 @@ public class GameArena
                 max = area.getMax();
                 spawn = area.getSpawnLoc();
             }
+            // Устанавливаем значения в конфиге.
             if (min != null)
                 Configurations.saveLocationAsString (GameArenas.getConfig(), "arenas." + arenaName + ".teams." + teamColor + ".min", min);
             if (max != null)
@@ -119,12 +135,12 @@ public class GameArena
         this.teamSize = teamSize;
         this.maxPlayers = teams.size()*this.teamSize;
     }
-
+    
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) return this == null;
+        if (obj == null) return false;
         if (this == obj) return true;
-        if (obj.getClass() == getClass()) return false;
+        if (obj.getClass().equals(getClass())) return false;
 
         GameArena arena = (GameArena)obj;
         return arena.getArenaName().equalsIgnoreCase(this.arenaName);
