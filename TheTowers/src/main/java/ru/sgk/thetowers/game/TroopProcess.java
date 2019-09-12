00e51bf8fa22c6c47f8c363ce7b8deb69b96a020
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-13 01:27:34 +0700

    TroopProcess.java Комментарии и прочее
    - Комментарии
    - Добавил очередь для спавна мобов, что бы не происходило такого, что все мобы заспавнятся одновременно и они будут как один бегать

--- TheTowers/src/main/java/ru/sgk/thetowers/game/TroopProcess.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/game/TroopProcess.java
@@ -1,50 +1,100 @@
 package ru.sgk.thetowers.game;
 
-import com.google.common.collect.Lists;
-import org.bukkit.Bukkit;
-import ru.sgk.thetowers.MainTowers;
-import ru.sgk.thetowers.game.data.teams.GameTeam;
-import ru.sgk.thetowers.game.data.troops.AbstractTroop;
-
 import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
+
+import org.bukkit.Bukkit;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Queues;
+
+import ru.sgk.thetowers.MainTowers;
+import ru.sgk.thetowers.game.data.troops.AbstractTroop;
 
 public class TroopProcess
 {
+	/** Список добвыленных мобов */
     private static List<AbstractTroop> troops = Collections.synchronizedList(Lists.newArrayList());
+	/** Список мобов для их удаления из списка выше*/
     private static List<AbstractTroop> remove = Collections.synchronizedList(Lists.newArrayList());
+    private static Queue<AbstractTroop> troopsQueue = Queues.newSynchronousQueue();  
+    /** Айди потока, который обрабатывает мобов */
     private static int taskId = -1;
-
-    synchronized public static void add(AbstractTroop troop, GameTeam team)
+    /** Частота, с которой будут спавниться мобы */
+    private static int frequency = 5;
+    /** Таймер для подсчёта прошедших тиков */
+    private static int timer = 0;
+    /**
+     * Добавляет моба в очередь на спавн
+     * @param troop - моб, которого нужно добавить в очередь
+     */
+    synchronized public static void add(AbstractTroop troop)
     {
+    	if (troop == null)
+    		throw new NullPointerException("cannot add troop which is null element");
+    	// Если такой моб уже находится в списке, либо в очереди на спавн, то пропускаем добавление его в очередь.
         for (AbstractTroop t : troops)
         {
             if (t.getEntity().equals(troop.getEntity()) || t.equals(troop))
                 return;
         }
-        troops.add(troop);
-        troop.spawn();
-        if (troops.size() == 1 && taskId == -1)
+        for (AbstractTroop t : troopsQueue) 
+        {
+        	if (t.equals(troop) || t.getEntity().equals(troop.getEntity()))
+        		return;
+        }
+        // Добавляем моба в очередь на спавн
+        troopsQueue.add(troop);
+        // Если обработчик не запущен - запускаем его  
+        if (taskId == -1)
             Bukkit.getScheduler().scheduleSyncRepeatingTask(MainTowers.getInstance(), TroopProcess::update, 0, 1);
     }
+    
+    /**
+     * Обновление мобов
+     */
     public static void update()
     {
+    	// Если таймер равен частоте, то спавним моба из очереди
+    	if (timer == frequency)
+    	{
+    		
+    		if (troopsQueue.peek() != null)
+    		{
+    			troopsQueue.peek().spawn();
+    			troops.add(troopsQueue.poll());
+    		}
+    		
+    		timer = 0;
+    	}
         for (AbstractTroop troop : troops)
         {
+        	// Обновляем моба
             troop.update();
+            // Если после обновления с ним что-то случилось, то добавляем его в список для удаления
             if (troop.isKilled() || troop.isDespawned())
                 remove(troop);
         }
+        // Подчищаем лишнее
         removeAll();
+        // Обновляем таймер
+        timer++;
     }
-    public static void remove(AbstractTroop troop)
+    /**
+     * Добавляет моба в remove список  
+     * @param troop - моб, которого нужно будет удалить.
+     */
+    synchronized public static void remove(AbstractTroop troop)
     {
         remove.add(troop);
     }
-    private static void removeAll()
+    /** Удаляет мобов из списка remove */
+    synchronized private static void removeAll()
     {
         try
         {
+        	// Удаляем из списка мобов тех, которые присутствуют в списке remove
             troops.removeAll(remove);
         }
         catch(Exception e)
@@ -52,10 +102,17 @@ public class TroopProcess
             e.printStackTrace();
         }
         remove.clear();
-        if (troops.size() == 0 && taskId != -1)
+        if (troops.size() == 0 && troopsQueue.size() == 0 && taskId != -1)
         {
+        	// Останавливаем поток.
             Bukkit.getScheduler().cancelTask(taskId);
+
+        	// Обнуляем всё.
             taskId = -1;
+            timer = 0;
+
+            troopsQueue.clear();
+            troops.clear();
         }
     }
 }
