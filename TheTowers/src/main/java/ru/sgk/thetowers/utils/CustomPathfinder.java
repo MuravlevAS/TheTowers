7f7699080c1942a02c57597273b370dc1f2327d5
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-12 15:21:44 +0700

    Update CustomPathfinder.java

--- TheTowers/src/main/java/ru/sgk/thetowers/utils/CustomPathfinder.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/utils/CustomPathfinder.java
@@ -1,10 +1,9 @@
 package ru.sgk.thetowers.utils;
 
-import net.minecraft.server.v1_14_R1.EntityInsentient;
-import net.minecraft.server.v1_14_R1.Navigation;
-import net.minecraft.server.v1_14_R1.PathEntity;
-import net.minecraft.server.v1_14_R1.PathfinderGoal;
 import org.bukkit.Location;
+
+import net.minecraft.server.v1_14_R1.EntityInsentient;
+import net.minecraft.server.v1_14_R1.PathfinderGoal;
 
 public class CustomPathfinder extends PathfinderGoal {
 
@@ -27,7 +26,6 @@ public class CustomPathfinder extends PathfinderGoal {
     @Override
     public void c()
     {
-        PathEntity pathEntity = entity.getNavigation().a(loc.getX(), loc.getY(), loc.getZ(),0);
 
         this.entity.getNavigation().a(loc.getX(), loc.getY(), loc.getZ(), speed);
 
