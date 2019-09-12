0b2a160b6bcef7982066e03a52ff8f32beef5c6b
MuravlevAS <43071478+Muravchik@users.noreply.github.com>
2019-09-12 22:27:19 +0700

    Configurations.java Комментарии

--- TheTowers/src/main/java/ru/sgk/thetowers/data/Configurations.java
+++ TheTowers/src/main/java/ru/sgk/thetowers/data/Configurations.java
@@ -11,38 +11,76 @@ import org.bukkit.configuration.file.FileConfiguration;
 import com.google.common.collect.Lists;
 
 import ru.sgk.thetowers.utils.Logs;
-
-public class Configurations {
+/**
+ * Конфиги
+ * @author SteveGrKek
+ */
+public class Configurations 
+{
+	/** config.yml */
 	private static FileConfiguration config = null;
+	/** locale_[lang].yml */
 	private static FileConfiguration locale = null;
+	/** config.yml */
 	private static FileConfiguration settings = null;
+	private static String localeString;
 
-
+	/**
+	 * Переводит Строку вида {world}, {x}, {y}, {z} объект Location с соответствующими значениями 
+	 * @param s - строка, которая будет переведена в локацию  
+	 * @return - локацию с соответствующими значениями world, x, y, z; null, если строка s равна null или если при чтении строки возникла ошибка
+	 */
 	private static Location stringToLoc(String s)
 	{
+		if (s == null) return null;
 		try
 		{
+			// Разделяем строку на массив.
+			// Массив должен содержать 4 элемента
+			// 1. world name 
+			// 2. Координата x
+			// 3. Координата y
+			// 4. Координата z
 			String[] locationFields = s.split(",");
-
+			// Получаем мир из строки
 			World world = Bukkit.getWorld(locationFields[0].replaceAll(" ", ""));
-
+			// Выводим сообщение об ошибке, если мир не найден.
+			if (world == null)
+			{
+				Logs.send("§cCannot convert string " + s + " to Location object.");
+				Logs.send("World with name " + locationFields[0].replaceAll(" ", "") + " is not exist!");
+				return null;
+			}
+			// получаем координаты 
 			double x = Double.parseDouble(locationFields[1].replaceAll(" ", ""));
 			double y = Double.parseDouble(locationFields[2].replaceAll(" ", ""));
 			double z = Double.parseDouble(locationFields[3].replaceAll(" ", ""));
-
+			// Создаём объект Location и возвращаем его. 
 			return new Location(world, x, y, z);
 		}
-		catch(Exception e){ }
+		catch(Exception e){
+			Logs.send("§c&lCannot convert string \"" + s + "\" to Location object. Wrong format.");
+			e.printStackTrace();
+		}
 		return null;
 	}
+	
+	/**
+	 * Переводит локацию <i>loc</i> в строку вида <i>{world}, {x}, {y}, {z}</i>
+	 * @param loc локация, которую нужно преобразовать в строку
+	 * @return - строку с соотвутствующую описанию; null если локация равна null.
+	 */
 	private static String locToString(Location loc)
 	{
 		if (loc == null) return null;
+		
 		World world = loc.getWorld();
 		String worldString = world.getName();
+		
 		double x = loc.getX();
 		double y = loc.getY();
 		double z = loc.getZ();
+		
 		return worldString	.concat(", ").concat(String.valueOf(x))
 				.concat(", ").concat(String.valueOf(y))
 				.concat(", ").concat(String.valueOf(z));
@@ -54,31 +92,59 @@ public class Configurations {
 	 *
 	 * @param config конфиг, из которого загружаем
 	 * @param path путь в конфиге, откуда загружать локацию
-	 * @return объект Location созданный при помощи new Location(worldname, x, y, z)
+	 * @return объект Location созданный при помощи new Location(worldname, x, y, z); null, если переменная в конфиге не найдена
 	 */
 	public static Location getLocation(FileConfiguration config, String path)
 	{
 		if (!config.contains(path)) return null;
+		
 		String s = config.getString(path);
+		
 		return stringToLoc(s);
 	}
-	public static List<Location> getLocationList(FileConfiguration config, String path)
+	
+	/**
+	 * Преобразует массив строк из конфига с помощью метода {@link #stringToLoc(String) stringToLock()} в массив с локациями
+	 * @param config - конфиг из которого нужно получить список
+	 * @param path - путь к списку в конфиге
+	 * @return список из объектов Location; 
+	 * @throws NullPointerException если не найдена строка в конфиге
+	 */
+	public static List<Location> getLocationList(FileConfiguration config, String path) throws NullPointerException
 	{
 		List<String> stringList = config.getStringList(path);
 		List<Location> locationList = Lists.newArrayList();
+		
 		for (String str : stringList)
 		{
-			locationList.add(stringToLoc(str));
+			Location l = stringToLoc(str);
+			// Если stringToLoc выдаёт ошибку, он выведет её в консоль.
+			// Просто пропускаем элемент 
+			if (l != null)
+			locationList.add(l);
 		}
 		return locationList;
 	}
-
-	public static void saveLocationList(FileConfiguration config, String path, List<Location> locList)
+	/**
+	 * Сохраняет список локаций в конфиг
+	 * @param config - конфиг, в который нужно сохранить список
+	 * @param path - путь к списку в конфиге
+	 * @param locList - список локаций, который надо сохранить
+	 * @throws NullPointerException если какой-то из аргументво равен null 
+	 */
+	public static void saveLocationList(FileConfiguration config, String path, List<Location> locList) throws NullPointerException
 	{
+		// Если один их аргументов равен null, то выбрасываем исключение. 
+		if (config == null || locList == null || path == null)
+			throw new NullPointerException();
 		List<String> stringList = Lists.newArrayList();
 		for (Location loc : locList)
 		{
-			stringList.add(locToString(loc));
+			String s = locToString(loc);
+			// Если locToString выдаёт ошибку, он выведет её в консоль.
+			// Просто пропускаем элемент 
+			if (s != null)
+			stringList.add(s);
 		}
 		config.set(path, stringList);
 	}
@@ -95,10 +161,18 @@ public class Configurations {
 		Logs.sendDebugMessage("saving location ".concat(saveString));
 		config.set(path, saveString);
 	}
-
+	/**
+	 * Получает строку из файла с локализацией 
+	 * @param path - путь в конфиге 
+	 * @return
+	 */
 	public static String getLocaleString(String path){
 		return getLocale().getString(path).replaceAll("%prefix%", getLocale().getString("prefix")).replaceAll("&", "§");
 	}
+	/**
+	 * Поулчает список строк из файла с локализацией 
+	 * @param path - путь в конфиге
+	 */
 	public static List<String> getLocaleStringList(String path){
 		List<String> l1 = new ArrayList<>();
 		List<String> l2 = getLocale().getStringList(path);
@@ -108,6 +182,10 @@ public class Configurations {
 		}
 		return l1;
 	}
+	
+	/**
+	 * Загружает файл конфигурации config.yml для последующего его получения с помощью {@link #getConfig() getConfig()}
+	 */
 	public static void loadConfig()
 	{
 		config = Configuration.loadConfig("config.yml");
@@ -115,30 +193,36 @@ public class Configurations {
 
 	/**
 	 * @throws NullPointerException if config was not loaded before
-	 * @return
+	 * @return config.yml
 	 */
 	public static FileConfiguration getConfig()
 	{
-		if (config == null) throw new NullPointerException("Configuration file is null"); 
+		if (config == null) loadConfig(); 
 		return config;
 	}
-
+	/**
+	 * Перезагружает конфиг.
+	 * По сути, просто вызывает метод {@link #loadConfig() loadConfig()}
+	 */
 	public static void relaodConfig()
 	{
 		loadConfig();
 	}
 
 	/**
-	 * @throws NullPointerException if config was not loaded before
+	 * Сохраняет конфиг config.yml
+	 * @throws NullPointerException если конфиг не был ранее загружен 
 	 */
-	public static void saveConfig()
+	public static void saveConfig() throws NullPointerException
 	{
 		if (config == null) throw new NullPointerException();
 		Configuration.saveConfiguration(config, "config.yml");
 	}
 
 	/**
-	 * @throws NullPointerException if messages was not loaded before
+	 * Возвращает объект типа FileConfiguration соответствующий locale_[lang].yml<br>
+	 * <b>lang</b> - язык, который указывается при загрузке локализаций методом {@link #loadLocale(String) loadLocale(lang)}
+	 * @throws NullPointerException если локализация не была ранее загружена
 	 */
 	public static FileConfiguration getLocale()
 	{
@@ -146,17 +230,20 @@ public class Configurations {
 		return locale;
 	}
 	
-	public static void loadLocale(String lang)
+	/**
+	 * Загружает лоализацию с языком lang
+	 * @param lang - язык (:P)
+	 * @throws NullPointerException если не найден файл с именем <i>locale_[lang].yml</i>
+	 */
+	public static void loadLocale(String lang) throws NullPointerException
 	{
 		locale = Configuration.loadConfig("locale_"+lang+".yml");
+		if (locale == null) throw new NullPointerException("Locale file is null");
+		localeString = lang;
 	}
 	
-	public static void reloadLocale(String lang)
-	{
-		loadLocale(lang);
-	}
-
 	/**
+	 * Сохраняет файл локализации
 	 * @throws NullPointerException if messages was not loaded before
 	 */
 	public static void saveLocale(String lang)
@@ -164,32 +251,38 @@ public class Configurations {
 		if (locale == null) throw new NullPointerException();
 		Configuration.saveConfiguration(locale, "locale_"+lang+".yml");
 	}
-	
+	/**
+	 * Загружает файл с настройками таверов и мобов
+	 */
 	public static void loadSettings()
 	{
 		settings = Configuration.loadConfig("settings.yml");
 	}
-	
-	public static void relaodSettings()
-	{
-		loadSettings();
-	}
 
 	/**
-	 * @throws NullPointerException if settings was not loaded before
+	 * Получает настройки таверов и мобов. Если конфиг ещё не был загружен, он загружается с помощью {@link #loadSettings() loadSettings()}
 	 */
 	public static FileConfiguration getSettings()
 	{
-		if (settings == null) throw new NullPointerException();
+		if (settings == null) loadSettings();
 		return settings;
 	}
 
 	/**
+	 * Сохраняет настройки таверов и мобов в файл settings.yml
 	 * @throws NullPointerException if settings was not loaded before
 	 */
 	public static void saveSettings()
 	{
 		if (settings == null) throw new NullPointerException();
 		Configuration.saveConfiguration(settings, "settings.yml");
+	}
+
+	/**
+	 * @return строку, соответствующую языку локализации, которая была ранее загружена методом {@link #loadLocale(String) loadLocale()}
+	 */
+	public static String getLocaleString() 
+	{
+		return localeString;
 	}
 }
